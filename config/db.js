eaeda6f5c35b3333bf9589fee3b500e231ce69a5
martinoremonje <m.monjeorellana@gmail.com>
2024-06-18 11:15:31 -0400

    arreglo menor a config

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const {DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_PORT} = process.env;
 const config = {
     host: DB_HOST,
     port: DB_PORT,
-    user: "postgres",
+    user: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_DATABASE,
     allowExitOnIdle: true
